6de61bcf466d60fb4c80c4ed36718244d9f234ad
Antonis Sideris <antonisid@gmail.com>
2020-09-03 21:01:26 +0200

    Recover the channels & console php files

---
+++ tests/Feature/UserControllerTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\Response;
+use Tests\TestCase;
+
+class UserControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * Test register()
+     */
+    public function testRegister(): void
+    {
+        $user = factory(User::class)->make();
+
+        $response = $this->json('POST', '/api/register', [
+            'name' => $user->name,
+            'email' => $user->email,
+            'password' => $user->password,
+            'password_confirmation' => $user->password
+        ]);
+
+        $response->assertStatus(Response::HTTP_CREATED);
+        $response->assertSee('token');
+    }
+
+    /**
+     * Test login()
+     */
+    public function testLogin(): void
+    {
+        $user = factory(User::class)->make();
+
+        $this->json('POST', '/api/register', [
+            'name' => $user->name,
+            'email' => $user->email,
+            'password' => $user->password,
+            'password_confirmation' => $user->password
+        ]);
+
+        $response = $this->json('POST', '/api/login', [
+            'email' => $user->email,
+            'password' => $user->password,
+        ]);
+
+        $response->assertStatus(Response::HTTP_OK);
+        $response->assertSee('token');
+    }
+
+    /**
+     * Test register()
+     */
+    public function testCannotRegisterWhenInputValidationFails(): void
+    {
+        $user = factory(User::class)->make(['email' => 'testdemo.com']);
+
+        $response = $this->json('POST', '/api/register', [
+            'name' => $user->name,
+            'email' => $user->email,
+            'password' => $user->password,
+            'password_confirmation' => $user->password
+        ]);
+
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+        $response->assertDontSee('token');
+    }
+}
