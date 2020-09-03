1c720db740e68eed7b383081349d4558af8504f3
Antonis Sideris <antonisid@gmail.com>
2020-09-03 10:27:02 +0200

    Modify the userFactory and remove reduntant migration files

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -10,11 +10,6 @@ use Illuminate\Support\Str;
 |--------------------------------------------------------------------------
 | Model Factories
 |--------------------------------------------------------------------------
-|
-| This directory should contain each of the model factory definitions for
-| your application. Factories provide a convenient way to generate new
-| model instances for testing / seeding your application's database.
-|
 */
 
 $factory->define(User::class, function (Faker $faker) {
@@ -22,7 +17,7 @@ $factory->define(User::class, function (Faker $faker) {
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'password' => Str::random(8),
         'remember_token' => Str::random(10),
     ];
 });
