6d98415568da4cc7b81294f9dce77bcaa256ffa5
Antonis Sideris <antonisid@gmail.com>
2020-09-05 18:19:38 +0200

    Define the stucture for listing servers and write test

--- tests/Feature/ServerControllerTest.php
+++ tests/Feature/ServerControllerTest.php
@@ -6,8 +6,10 @@ namespace Tests\Feature;
 use App\Server;
 use App\Transformers\ServerTransformer;
 use App\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Artisan;
 use Tests\TestCase;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
@@ -24,8 +26,48 @@ class ServerControllerTest extends TestCase
     {
         parent::setUp();
 
+        //Artisan::call('db:seed', ['--class' => 'ServerSeeder']);
+
         $user = factory(User::class)->create();
+
         $this->token = JWTAuth::fromUser($user);
+    }
+
+    /**
+     * Test index()
+     */
+    public function testGetServers(): void
+    {
+        $server = factory(Server::class)->create();
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $this->token,
+        ])->json('GET', '/api/assets');
+
+        $response->assertStatus(Response::HTTP_OK);
+
+        $response->assertJson([
+            'items' => [
+                [
+                    'id' => $server->getId(),
+                    'model' => $server->getModel(),
+                    'hdd' => [
+                        'description' => $server->getHdd(),
+                        'capacity' => $server->getHddCapacity(),
+                        'unit' => ServerTransformer::GB_UNIT
+                    ],
+                    'ram' => [
+                        'description' => $server->getRam(),
+                        'capacity' => $server->getRamCapacity(),
+                        'unit' => ServerTransformer::GB_UNIT
+                    ],
+                    'location' => $server->getLocation(),
+                    'price' => $server->getPrice(),
+                    'created_at' => $server->getCreatedAt(),
+                    'updated_at' => $server->getUpdatedAt()
+                ]
+            ]
+        ]);
     }
 
     /**
